2a8c4f6d3b2c6f491d05ac5764048f93cc8097af
TUANTMA-PC\Phuong.tt <phuong.tt@aris-vn.com>
2022-03-01 13:06:58 +0700

    gfjdg

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import './App.css';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
+function App() {
+  const [isPlay, setIsPlay] = useState(false)
+  const [isEnd, setIsEnd] = useState(false)
+  const getAudio = useRef(null)
+  const [currentTimePlayer, setCurrentTimePlayer] = useState("0:00")
+  const [durationPlayer, setDurationPlayer] = useState(0)
+  const getProgressBar = useRef(null)
+  const now = useRef(null)
+
+  const ahuhu = () => {
+    const song = getAudio.current
+    setDurationPlayer(formatNumber(Math.ceil(song.duration)))
+    song?.addEventListener("timeupdate", ()=>{
+      let time = formatNumber(Math.ceil(song.currentTime))
+      setCurrentTimePlayer(time)
+      now.current.style.width = song.currentTime / song.duration.toFixed(3) * 100 + '%'
+      if(song.currentTime === song.duration) setIsEnd(true)
+    })
+  }
+
+  function formatNumber(s){return (s-(s%=60))/60+(9<s?':':':0')+s}
+
+  const handlePlay = () => {
+    if(!isPlay){
+      getAudio.current.play()
+      setIsPlay(true)
+      if(isEnd===true) setIsEnd(false)
+    }
+    else{
+      getAudio.current.pause()
+      setIsPlay(false)
+    }
+     
+  }
+
+  const next = () => {
+    let time = getAudio.current
+    getAudio.current.currentTime = Math.ceil(time.currentTime)+5
+  }
+
+  const previous = () => {
+    let time = getAudio.current
+    getAudio.current.currentTime = Math.ceil(time.currentTime)-5
+  }
+  
+  isPlay&&( getProgressBar.current?.addEventListener('click', function (event) {
+    let coordStart = this.getBoundingClientRect().left
+    let coordEnd = event.pageX
+    let p = (coordEnd - coordStart) / this.offsetWidth
+    now.current.style.width = p.toFixed(3) * 100 + '%'
+    getAudio.current.currentTime = p * getAudio.current.duration
+  }))
+  
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        <div className='playerContainer'>
+          <div className='playerBody'>
+            <div className='imgPlayer'>
+              <img src={process.env.PUBLIC_URL + "/img/sky.jpg"}></img>
+            </div>
+            <div className='controlPlayer'>
+              <audio ref={getAudio} onLoadedData={ahuhu}>
+                <source src={process.env.PUBLIC_URL + "/audio/flyingWithoutWings.mp3"} type="audio/mpeg"></source>
+              </audio>
+              <div className='btnGroup'>
+                <FontAwesomeIcon icon={faBackward} onClick={previous} />
+                <FontAwesomeIcon icon={(isPlay&&!isEnd)?faPause:faPlay} onClick={handlePlay}/>
+                <FontAwesomeIcon icon={faForward} onClick = {next}/>
+              </div>
+              <div className='timeControl'>
+                <span>{currentTimePlayer}</span>
+                <span>{durationPlayer}</span>
+              </div>
+              <div className='controlProgress' ref={getProgressBar}>
+                <div className='now' ref={now}></div>
+                <div className='musicDuration'></div>   
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
